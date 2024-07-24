52f9562c526f38ff7e1c9a25a8a4cd15057959c4
Aubrey Kurten <aekurten@gmail.com>
2024-07-24 20:50:27 +0200

    Added the boot data & created the UI

--- reway/components/Similarproducts.jsx
+++ reway/components/Similarproducts.jsx
@@ -1,3 +1,6 @@
+import { bootData } from "@/constants/data";
+import { faArrowRight, faStoreAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Similarproducts = () => {
@@ -8,6 +11,25 @@ const Similarproducts = () => {
           more from <span className="text-rewayBlue">boots</span>
         </p>
         <div className="h-[.5px] bg-rewayBlack w-full"></div>
+      </div>
+      <div className="flex gap-4 flex-wrap">
+        {bootData.map((boot) => {
+          return (
+            <div className="w-[32%] h-72 flex flex-col">
+              <img src={boot.image} alt="" />
+              <p>{boot.title}</p>
+              <div className="flex items-center gap-2">
+                <FontAwesomeIcon icon={faStoreAlt} />
+                <p>{boot.brand}</p>
+              </div>
+              <p>R{boot.price}</p>
+              <div className="flex items-center gap-2 self-end">
+                <p>View</p>
+                <FontAwesomeIcon icon={faArrowRight} />
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- reway/constants/data.js
+++ reway/constants/data.js
@@ -8,7 +8,50 @@ import {
 const navItems = ["Apparels", "shoes", "equipment", "gift"];
 const navIcons = [faGift, faStar, faHeart, faTag];
 
-export { navItems, navIcons };
+const bootData = [
+  {
+    image: "/images/Boots05.png",
+    isMarkedDown: true,
+    title: "Hargrave ankle boot",
+    brand: "Superbalist",
+    price: "500",
+    isLowStock: false,
+  },
+  {
+    image: "/images/Boots03.png",
+    isMarkedDown: false,
+    title: "Cameron Leather",
+    brand: "Superbalist",
+    price: "539",
+    isLowStock: true,
+  },
+  {
+    image: "/images/Boots01.png",
+    isMarkedDown: true,
+    title: "Grayson Brogue",
+    brand: "Superbalist",
+    price: "420",
+    isLowStock: true,
+  },
+  {
+    image: "/images/Boots04.png",
+    isMarkedDown: true,
+    title: "Orca",
+    brand: "Jack and Jones",
+    price: "500",
+    isLowStock: true,
+  },
+  {
+    image: "/images/Boots02.png",
+    isMarkedDown: false,
+    title: "Jean Hiker Boot",
+    brand: "Superbalist",
+    price: "600",
+    isLowStock: false,
+  },
+];
+
+export { navItems, navIcons, bootData };
 
 //Create mock db with products?
 //create data object with product info
