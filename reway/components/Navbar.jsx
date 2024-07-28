c63ad8b86860f0802d00c1d5c1f8328598b79ed3
Aubrey Kurten <aekurten@gmail.com>
2024-07-28 18:17:47 +0200

    Added a mobile nav

--- reway/components/Navbar.jsx
+++ reway/components/Navbar.jsx
@@ -11,6 +11,7 @@ import useMidDeviceCheck from "@/hooks/checkMidDevice";
 
 const Navbar = () => {
   const [isActive, setIsActive] = useState(null);
+  const [isMobileMenuActive, setIsMobileMenuActive] = useState(false);
   const isMobile = useMobileCheck();
   const isMidDevice = useMidDeviceCheck();
 
@@ -23,11 +24,58 @@ const Navbar = () => {
   };
 
   return (
-    <div className="px-11 py-6 flex items-center justify-between ">
+    <div className="px-11 py-6 flex items-center justify-between relative">
       {isMobile && (
         <div className="flex items-center justify-between w-full">
-          <FontAwesomeIcon icon={faBars} size="2x" />
-          <Image src={logo} width={120} height={40} alt="logo" />
+          <div className="flex items-center justify-between w-full">
+            <FontAwesomeIcon
+              icon={faBars}
+              size="2x"
+              onClick={() => setIsMobileMenuActive(!isMobileMenuActive)}
+            />
+            <Image src={logo} width={120} height={40} alt="logo" />
+          </div>
+          {isMobileMenuActive && (
+            <div className=" flex-col absolute left-16 -bottom-80 bg-rewayWhite p-4 rounded-md z-[1000]">
+              {navItems.map((item, index) => (
+                <p
+                  key={item}
+                  className={`font-suez font-semibold text-[17px] uppercase hover:cursor-pointer mt-4 hover:text-rewayBlue transition-all duration-300 ${
+                    isActive === index ? `text-rewayBlue` : ``
+                  }`}
+                  onClick={() => toggleActiveState(index)}
+                >
+                  {item}
+                  <hr />
+                </p>
+              ))}
+              <div className="flex gap-8 mt-6">
+                {navIcons.map((icon) => (
+                  <FontAwesomeIcon
+                    key={icon}
+                    icon={icon}
+                    size="lg"
+                    className="hover:text-rewayBlue hover:cursor-pointer transition-all duration-300"
+                  />
+                ))}
+              </div>
+              <div className="flex gap-8 items-center mt-8">
+                {/* Create active/hover states */}
+                <Link
+                  href="#"
+                  className="uppercase text-rewayBlue font-bold font-suez"
+                >
+                  sign up
+                </Link>
+                <Link
+                  href="#"
+                  className="uppercase text-rewayBlue font-bold border-2 border-rewayBlue px-4 py-2 rounded-[3px] font-suez"
+                >
+                  Login
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
       )}
       {!isMobile && (
