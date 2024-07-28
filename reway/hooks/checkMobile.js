4297b7e4b8a43b1f71d4030964c4d0caff617c8c
Aubrey Kurten <aekurten@gmail.com>
2024-07-28 15:42:24 +0200

    Still looking for the fix ...

--- reway/hooks/checkMobile.js
+++ reway/hooks/checkMobile.js
@@ -1,22 +1,27 @@
 "use client";
 import { useState, useEffect } from "react";
 
-// Custom hook to check if the device is mobile
+// A check to see if the device is a mobile device
 const useMobileCheck = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    // Check if window is defined to ensure the code is running on the client
     if (typeof window !== "undefined") {
       const handleResize = () => {
         setIsMobile(window.innerWidth < 720);
       };
 
-      handleResize(); // Initial check
+      // Initial check
+      handleResize();
+
+      // Add resize event listener
       window.addEventListener("resize", handleResize);
 
+      // Cleanup event listener on unmount
       return () => window.removeEventListener("resize", handleResize);
     }
-  }, []);
+  }, []); // Empty dependency array ensures this runs once
 
   return isMobile;
 };
